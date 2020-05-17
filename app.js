99f87a2a39a9954ebaef4d9ec8414724fda71db9
nforla <nicoforlano@protonmail.com>
2020-05-17 12:10:56 -0300

    Entorno y Conexión con Mongo

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGODB_URI,
+    {useNewUrlParser: true}
+).then(() => console.log('DB Connected!'));
+
+mongoose.connection.on('error', (err) => {
+    console.log('Something went wrong: ', err);      
+});
 
 const app = express();
 
